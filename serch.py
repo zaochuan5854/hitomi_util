c68d2da5bdcaa71c21a7c24b1fb5f72366095616
zaochuan5854 <zaochuantaiji338@gmail.com>
2025-05-26 13:06:22 +0900

    検索機能を開発中get_galleryids_for_query関数まで

---
+++ serch.py
@@ -0,0 +1,191 @@
+import io
+import time
+import struct
+import hashlib
+import requests
+import numpy as np
+import numpy.typing as npt
+from typing import Any
+from dataclasses import dataclass
+
+def get_index_version() -> str:
+    js_epoch_time: int = int(time.time() * 1000)
+    url = f'https://ltn.gold-usergeneratedcontent.net/galleriesindex/version?_={js_epoch_time}'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        index_version = response.text
+    except Exception as e:
+        print(f'Failed to parse index_version. error: {e}')
+        raise e
+    return index_version
+
+def get_url_at_range(url: str, range: tuple[int, int]) -> npt.NDArray[np.uint8]:
+    """サーバーからアドレス範囲を指定したindexファイルを取得する関数
+
+    Args:
+        url (str): おそらくhttps://ltn.gold-usergeneratedcontent.net/galleriesindex/galleries.{indexversion}.index
+        range (tuple[int, int]): 取得する.indexのアドレス範囲
+
+    Raises:
+        e: httpエラーもしくはunit8ではないレスポンスをparseしようとしたエラー
+
+    Returns:
+        uint8_array (npt.NDArray[np.uint8]): uint8_array化されたレスポンス
+    """
+    headers: dict[str, str] = {'Range': f'bytes={range[0]}-{range[1]}'}
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        
+        content_bytes = response.content
+        unit8_array = np.frombuffer(content_bytes, dtype=np.uint8)
+    except Exception as e:
+        print(f'Failed to parse uint8_array. error: {e}')
+        raise e
+    return unit8_array
+
+@dataclass
+class NodeData:
+    offset: np.uint64
+    length: np.int32
+
+@dataclass
+class Node:
+    """subnode_addressesの流さばおそらく17になる(https://ltn.gold-usergeneratedcontent.net/searchlib.js のconst Bより)
+    """
+    keys: list[npt.NDArray[np.uint8]]
+    datas: list[NodeData]
+    subnode_addresses: list[np.uint64]
+
+def read_int_or_uint_N_big_endian_from_adarray8(data: npt.NDArray[np.uint8], dtype: type, position: int) -> Any:
+    dtype_str = str(dtype)#例: <class 'numpy.uint8'>
+    if 'uint' in dtype_str:
+        signed = False
+    elif 'int' in dtype_str:
+        signed = True
+    else:
+        raise ValueError(f'Invalid dtype: {dtype_str}')
+    buffer = io.BytesIO(data.tobytes())
+    read_byte = np.dtype(dtype).itemsize
+    if read_byte == 1:
+        format_suffix= 'b' if signed else 'B'
+    elif read_byte == 2:
+        format_suffix= 'h' if signed else 'H'
+    elif read_byte == 4:
+        format_suffix= 'i' if signed else 'I'
+    elif read_byte == 8:
+        format_suffix= 'q' if signed else 'Q'
+    else:
+        raise ValueError(f'Unsupported byte length: {read_byte}')
+    buffer.seek(position)
+    result = struct.unpack('>'+format_suffix, buffer.read(read_byte)) # type: ignore
+    return dtype(result[0])
+
+def decode_node(data: npt.NDArray[np.uint8]) -> Node:
+    pos: int = 0
+    
+    keys: list[npt.NDArray[np.uint8]] = []
+    number_of_keys = read_int_or_uint_N_big_endian_from_adarray8(data, np.int32, pos)
+    pos += 32//8
+    for _ in range(number_of_keys):
+        key_size = read_int_or_uint_N_big_endian_from_adarray8(data, np.int32, pos)
+        if (not key_size) or (key_size > 32):
+            raise ValueError('Failed to parse key_size\nkey_size must be 32 or less')
+        pos += 32//8
+        keys.append(data[pos: pos+key_size])
+        pos += key_size
+    
+    datas: list[NodeData] = []
+    number_of_datas = read_int_or_uint_N_big_endian_from_adarray8(data, np.int32, pos)
+    pos += 32//8
+    for _ in range(number_of_datas):
+        offset = read_int_or_uint_N_big_endian_from_adarray8(data, np.uint64, pos)
+        pos += 64//8
+        length = read_int_or_uint_N_big_endian_from_adarray8(data, np.int32, pos)
+        pos += 32//8
+        node_data = NodeData(offset, length)
+        datas.append(node_data)
+    
+    subnode_addresses: list[np.uint64] = []
+    number_of_subnode_addresses = 17#https://ltn.gold-usergeneratedcontent.net/searchlib.js の const B = 16から
+    for _ in range(number_of_subnode_addresses):
+        subnode_addresse = read_int_or_uint_N_big_endian_from_adarray8(data, np.uint64, pos)
+        pos += 64//8
+        subnode_addresses.append(subnode_addresse)
+    
+    return Node(keys, datas, subnode_addresses)
+
+
+def get_node_at_address(field: str, address: np.uint64, galleries_index_version: str, serial: bool|None=None) -> Node:
+    #url = '//'+'ltn.gold-usergeneratedcontent.net'+'/'+'tagindex'+'/'+field+'.'+tag_index_version+'.index'
+    if (field == 'galleries'):
+        url = 'https://'+'ltn.gold-usergeneratedcontent.net'+'/'+'galleriesindex'+'/galleries.'+galleries_index_version+'.index'
+    else:
+        raise ValueError('開発途中です')
+    max_node_size = 464 #https://ltn.gold-usergeneratedcontent.net/searchlib.js の const max_node_size = 464から
+    address_int = int(address)
+    node_unit8 = get_url_at_range(url, (address_int, address_int+max_node_size-1))
+    node = decode_node(node_unit8)
+    return node
+
+def b_search(field: str, key: npt.NDArray[np.uint8], node: Node, galleries_index_version: str, serial: str|None=None) -> NodeData | bool:
+    
+    def _compare_arraybuffers(dv1: npt.NDArray[np.uint8], dv2: npt.NDArray[np.uint8]):
+        top: int = min(len(dv1), len(dv2))
+        for i in range(top):
+            if (dv1[i] < dv2[i]):
+                return -1
+            elif (dv1[i] > dv2[i]):
+                return 1            
+        return 0
+    
+    def _locate_key(key: npt.NDArray[np.uint8], node: Node) -> tuple[bool, int]:
+        comp_result_int = -1
+        i = 0
+        for _ in range(len(node.keys)):
+            comp_result_int = _compare_arraybuffers(key, node.keys[i])
+            if (comp_result_int <= 0):
+                break
+            i += 1
+        if (comp_result_int == 0):
+            comp_result = False
+        else:
+            comp_result = True
+        return (not comp_result, i)
+        
+    def _is_leaf(node: Node):
+        for node_subaddress in node.subnode_addresses:
+            if (node_subaddress):
+                return False
+        return True
+        
+    if not (len(node.keys)):
+        return False
+    
+    there, where = _locate_key(key, node)
+    
+    if (there):
+        return node.datas[where] #このreturnで終わらない下のb_serchが実行される
+    elif (_is_leaf(node)):
+        return False
+
+    if (node.subnode_addresses[where] == 0):
+        raise Exception('non-root node address 0')
+
+    return b_search(field, key, get_node_at_address(field, node.subnode_addresses[where], galleries_index_version), galleries_index_version)
+    
+
+def get_galleryids_for_query(query: str):
+    query_hashed = hashlib.sha256(query.encode()).digest()
+    key = np.frombuffer(query_hashed[:4], dtype=np.uint8)
+    field = 'galleries'
+    index_version: str = get_index_version()
+    
+    initinal_node = get_node_at_address(field, np.uint64(0), index_version)
+    
+    ans = b_search(field, key, initinal_node, index_version)
+    
+    print(ans)
+    
+get_galleryids_for_query('homunculus')
